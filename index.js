fa7f41282bf8f5811700e606d72c85e2cab1b155
Alex' Le Coq <alexandre.lcoq@gmail.com>
2019-10-03 15:47:03 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Jean-Bwouno'));
+console.log(chalk.yellow('Jean-Rodolphe'));
+console.log(chalk.red('Jean-Alexia'));
+console.log(chalk.green('Jean-Jean'));
+console.log(chalk.cyan('Jean-Eduardo'));
